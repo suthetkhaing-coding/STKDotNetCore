8058bf115ea4d1ca84fd4b5bd3bb6391418cf194
SuThetKhaing <suthetkhaing362@gmail.com>
2024-05-11 14:15:22 +0630

    [Complete] Burma Project Idea MyanmarProverbs JSON data to API

---
+++ STKDotNetCore.RestApiMyanmarProverbs/Controllers/MyanmarProverbsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace STKDotNetCore.RestApiMyanmarProverbs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyanmarProverbsController : ControllerBase
+    {
+        private async Task<MyanmarProverb> GetDataAsync()
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("data.json");
+            var model = JsonConvert.DeserializeObject<MyanmarProverb>(jsonStr);
+            return model;
+        }
+
+        [HttpGet("titles")]
+        public async Task<IActionResult> Titles()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.Tbl_MMProverbsTitle);
+        }
+
+        [HttpGet("proverbs")]
+        public async Task<IActionResult> Proverbs()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.Tbl_MMProverbs);
+        }
+
+        [HttpGet("{titleId}/{proverbId}")]
+        public async Task<IActionResult> Proverb(int titleId, int proverbId)
+        {
+            var model = await GetDataAsync();
+            var proverb = model.Tbl_MMProverbs.FirstOrDefault(x => x.TitleId == titleId && x.ProverbId == proverbId);
+
+            if (proverb == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(proverb);
+        }
+    }
+
+
+    public class MyanmarProverb
+    {
+        public Tbl_Mmproverbstitle[] Tbl_MMProverbsTitle { get; set; }
+        public Tbl_Mmproverbs[] Tbl_MMProverbs { get; set; }
+    }
+
+    public class Tbl_Mmproverbstitle
+    {
+        public int TitleId { get; set; }
+        public string TitleName { get; set; }
+    }
+
+    public class Tbl_Mmproverbs
+    {
+        public int TitleId { get; set; }
+        public int ProverbId { get; set; }
+        public string ProverbName { get; set; }
+        public string ProverbDesp { get; set; }
+    }
+
+}
