19feefc11ace8c7819175c1c0007c39a9fe884e3
SuThetKhaing <suthetkhaing362@gmail.com>
2024-06-22 14:32:35 +0630

    [Complete] ASP.NET Core MVC Blog Create

--- STKDotNetCore.MvcApp/Controllers/BlogController.cs
+++ STKDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using STKDotNetCore.MvcApp.Db;
+using STKDotNetCore.MvcApp.Models;
 
 namespace STKDotNetCore.MvcApp.Controllers
 {
@@ -17,6 +18,22 @@ namespace STKDotNetCore.MvcApp.Controllers
         {
             var lst = await _db.Blogs.ToListAsync();
             return View(lst);
+        }
+
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("BlogCreate");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public async Task<IActionResult> BlogCreate(BlogModel blog)
+        {
+            _db.Blogs.Add(blog);
+            var result = await _db.SaveChangesAsync();
+            //return View("BlogCreate");
+            return Redirect("/Blog");
         }
     }
 }
