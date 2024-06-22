87e6c55f6beb69e34d469c3bce963fcb2e42e426
SuThetKhaing <suthetkhaing362@gmail.com>
2024-06-22 15:07:52 +0630

    [Complete] ASP.NET Core MVC Blog Edit/Update, Delete

--- STKDotNetCore.MvcApp/Controllers/BlogController.cs
+++ STKDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -16,7 +16,9 @@ namespace STKDotNetCore.MvcApp.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var lst = await _db.Blogs.ToListAsync();
+            var lst = await _db.Blogs
+                .OrderByDescending(x => x.BlogId)
+                .ToListAsync();
             return View(lst);
         }
 
@@ -33,6 +35,52 @@ namespace STKDotNetCore.MvcApp.Controllers
             _db.Blogs.Add(blog);
             var result = await _db.SaveChangesAsync();
             //return View("BlogCreate");
+            return Redirect("/Blog");
+        }
+
+        [HttpGet]
+        [ActionName("Edit")]
+        public async Task<IActionResult> BlogEdit(int id)
+        {
+            var item = await _db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+            if (item is null)
+            {
+                return Redirect("/Blog");
+            }
+            return View("BlogEdit", item);
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public async Task<IActionResult> BlogUpdate(int id, BlogModel blog)
+        {
+            var item = await _db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+            if (item is null)
+            {
+                return Redirect("/Blog");
+            }
+
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+
+            await _db.SaveChangesAsync();
+            return Redirect("/Blog");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> BlogDelete(int id)
+        {
+            var item = await _db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+            if (item is null)
+            {
+                return Redirect("/Blog");
+            }
+
+            _db.Blogs.Remove(item);
+            await _db.SaveChangesAsync();
+
             return Redirect("/Blog");
         }
     }
