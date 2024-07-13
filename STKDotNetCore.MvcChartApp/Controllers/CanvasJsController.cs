2358522cc842e38cda0eaf21d7a0dd58e19439ee
SuThetKhaing <suthetkhaing362@gmail.com>
2024-07-13 18:31:38 +0630

    [Complete] CanvasJs Chart

---
+++ STKDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace STKDotNetCore.MvcChartApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+
+		public IActionResult AreaChart()
+		{
+			return View();
+		}
+
+        public IActionResult ColumnChart()
+        {
+            return View();
+        }
+    }
+}
