e10bd4e26fa8686fa4aa9a4013797a5139adfc88
SuThetKhaing <suthetkhaing362@gmail.com>
2024-09-08 23:30:20 +0630

    feat: Blog List

--- STKDotNetCore.RestApi/Program.cs
+++ STKDotNetCore.RestApi/Program.cs
@@ -2,9 +2,19 @@ using Microsoft.EntityFrameworkCore;
 using STKDotNetCore.MvcApp.Db;
 using STKDotNetCore.Shared;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localhost:7274",
+                                              "http://localhost:5100");
+                      });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -36,6 +46,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
+
 
 app.UseAuthorization();
 
