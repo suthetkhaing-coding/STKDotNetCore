cf2535cf8e7f1646c5f2df4b8e55624bbb824bd6
SuThetKhaing <suthetkhaing362@gmail.com>
2024-09-19 23:19:19 +0630

    feat: Blazor Server Blog CRUD

---
+++ STKDotNetCore.BlazorServer/Database/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using STKDotNetCore.BlazorServer.Models;
+
+namespace STKDotNetCore.BlazorServer.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
