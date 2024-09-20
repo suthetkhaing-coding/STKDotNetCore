4c8165b8dee35ad64cd918839a27ff5683d42757
SuThetKhaing <suthetkhaing362@gmail.com>
2024-09-20 22:45:05 +0630

    feat: Session, Middleware

---
+++ STKDotNetCore.MiddlewareApp/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using STKDotNetCore.MiddlewareApp.Models;
+
+namespace STKDotNetCore.MiddlewareApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginModel requestModel)
+        {
+            HttpContext.Session.SetString("name", requestModel.UserName);
+            return Redirect("/Home");
+        }
+    }
+}
---
+++ STKDotNetCore.MiddlewareApp/Controllers/LogoutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace STKDotNetCore.MiddlewareApp.Controllers
+{
+    public class LogoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpContext.Session.Clear();
+            return Redirect("/");
+        }
+    }
+}
